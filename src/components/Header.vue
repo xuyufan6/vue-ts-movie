81932e6710b7ccdd2eb3fe22558c11beeb9c9f7f
xuyufan6 <1067431030@qq.com>
2018-11-26 23:27:10 +0800

    动态修改title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <van-nav-bar title="首页" />
+  <div class="header">
+    <van-nav-bar :title="title" :left-arrow="back" @click-left="goBack" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 import { NavBar } from 'vant';
 
 @Component({
@@ -11,5 +13,33 @@ import { NavBar } from 'vant';
     [NavBar.name]: NavBar
   }
 })
-export default class Header extends Vue {}
+export default class Header extends Vue {
+  private title: string = '';
+  private back: boolean = false;
+
+  @Watch('$route.meta.title')
+  private onRouterTitleChange(val: string) {
+    this.title = val;
+    this.back = Boolean(val !== '首页');
+  }
+
+  private created() {
+    this.title = this.$route.meta.title;
+    this.back = Boolean(this.title !== '首页');
+  }
+
+  private goBack() {
+    this.$router.back();
+  }
+}
 </script>
+
+<style lang="scss">
+.van-nav-bar {
+  color: #fff;
+  background-color: #ee4000;
+  .van-icon {
+    color: #fff;
+  }
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import MovieDetail from './views/MovieDetail.vue';
 
 Vue.use(Router);
 
@@ -11,7 +12,18 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: '首页'
+      }
+    },
+    {
+      path: '/movieDetail/:id',
+      name: 'movieDetail',
+      component: MovieDetail,
+      meta: {
+        title: '电影详情'
+      }
     }
   ]
 });
